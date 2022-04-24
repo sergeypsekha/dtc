13dfe19eb426d077e1164a59a2df1c2df97e2822
SP <sergey.psekha@gmail.com>
2022-04-24 21:32:39 +0200

    added GetAllEventsFromRepo

--- dtc.api/Controllers/EventsController.cs
+++ dtc.api/Controllers/EventsController.cs
@@ -23,7 +23,27 @@ namespace dtc.api.Controllers
 
         private IEnumerable<Event> GetAllEventsFromRepo()
         {
-            throw new NotImplementedException();
+            return new List<Event> 
+            {
+                new Event()
+                {
+                    EventId = Guid.Parse("fb1087d6-437a-4163-a226-9368f32a0cf3"),
+                    TimeStamp = DateTime.Parse("2022-04-24T00:00:00.00000000"),
+                    EventType = "SearchView"
+                },
+                new Event()
+                {
+                    EventId = Guid.Parse("60f06190-198b-4e49-b415-e21be2344895"),
+                    TimeStamp = DateTime.Parse("2022-04-23T00:00:00.00000000"),
+                    EventType = "DetailsView"
+                },
+                new Event()
+                {
+                    EventId = Guid.Parse("80e1a755-5b05-4edc-855b-27188638225d"),
+                    TimeStamp = DateTime.Parse("2022-04-22T00:00:00.00000000"),
+                    EventType = "SearchView"
+                }
+            };
         }
     }
 }
