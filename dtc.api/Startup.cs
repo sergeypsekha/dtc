ba51acba7ebb2e840b19162951ef78e2d265c468
SP <sergey.psekha@gmail.com>
2022-04-24 21:16:03 +0200

    clean up
    clean up

--- dtc.api/Startup.cs
+++ dtc.api/Startup.cs
@@ -1,11 +1,11 @@
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace dtc.api
 {
