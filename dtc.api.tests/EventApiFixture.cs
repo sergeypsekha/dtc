e1f044bccad41adba428279359f28c71fe92f4df
SP <sergey.psekha@gmail.com>
2022-04-24 22:05:50 +0200

    implemented EventApiFixture;

--- dtc.api.tests/EventApiFixture.cs
+++ dtc.api.tests/EventApiFixture.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace dtc.api.tests
 {
     public class EventApiFixture : IDisposable
     {
+        private readonly IHost server;
+
+        public Uri ServerUri { get; }
+
+        public EventApiFixture()
+        {
+            this.ServerUri = new Uri("http://localhost:9222");
+            this.server = Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseUrls(this.ServerUri.ToString());
+                    webBuilder.UseStartup<TestStartup>();
+                })
+                .Build();
+
+            this.server.Start();
+        }
+
         public void Dispose()
         {
-            throw new NotImplementedException();
+            this.server.Dispose();
         }
     }
 }
