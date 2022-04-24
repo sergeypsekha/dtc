c61bf5aee715e5a09fee128a1bad53a457928691
SP <sergey.psekha@gmail.com>
2022-04-24 21:48:12 +0200

    added EventApiFixture and tidy up

---
+++ dtc.api.tests/EventApiFixture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dtc.api.tests
+{
+    public class EventApiFixture : IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
