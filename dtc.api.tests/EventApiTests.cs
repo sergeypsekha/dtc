e5dc3180f811dc1debdbe1c151efcd2913cb703d
SP <sergey.psekha@gmail.com>
2022-04-25 09:39:50 +0200

    added EventApiTests

---
+++ dtc.api.tests/EventApiTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace dtc.api.tests
+{
+    public class EventApiTests : IClassFixture<EventApiFixture>
+    {
+        private EventApiFixture fixture;
+        private ITestOutputHelper output;
+
+        public EventApiTests(EventApiFixture fixture, ITestOutputHelper output)
+        {
+            this.fixture = fixture;
+            this.output = output;
+        }
+    }
+}
